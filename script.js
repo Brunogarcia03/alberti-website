c38775a47af75ce04117324f0c99564b6d6fbba1
Brunogarcia03 <brunogalberti@gmail.com>
2025-06-26 18:55:13 -0300

    gsap hero

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const h1 = document.querySelector(".hero-video h1");
+  const h2 = document.querySelector(".hero h2");
+  const h5 = document.querySelector(".hero h5");
+  const header = document.querySelector("header");
+
+  document.body.style.overflow = "hidden";
+  document.documentElement.style.overflow = "hidden";
+
+  // Línea de tiempo
+  const tl = gsap.timeline({
+    ease: "power2.out",
+  });
+
+  tl.to(h1, {
+    fontSize: "calc(1vw + 1.5rem)",
+    duration: 2,
+    delay: 1.5,
+  })
+    .from(
+      "video",
+      {
+        width: "100%",
+        height: "100%",
+        marginBottom: "0vh",
+        borderRadius: "0px",
+        duration: 3,
+      },
+      "<"
+    )
+    .to(h1, {
+      opacity: 0,
+    })
+    .from(h2, { autoAlpha: 0, y: -15 })
+    .from(h5, { autoAlpha: 0, y: -15 })
+    .add(() => {
+      document.body.style.overflow = "";
+      document.documentElement.style.overflow = "";
+    })
+    .from(header, { autoAlpha: 0, y: "-100%" });
+});
